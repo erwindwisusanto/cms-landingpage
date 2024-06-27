48f241d6d36402ae76927df67db2f44b5b42018a
erwin dwi s <erwin@waki.mobi>
2024-06-27 15:56:46 +0700

    feat: layout dashboard

---
+++ resources/views/components/navbarcanvasoff.blade.php
@@ -0,0 +1,24 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="index.html">
+                <i class="mdi mdi-home menu-icon"></i>
+                <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <i class="mdi mdi-circle-outline menu-icon"></i>
+                <span class="menu-title">Websites</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Dengue</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Pharmacy</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Whitening</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</nav>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>landing page cms</title>
+
+    <link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/base/vendor.bundle.base.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
+</head>
+
+<body>
+    <div class="container-scroller">
+        <x-navbar />
+        <div class="container-fluid page-body-wrapper">
+            <x-navbarcanvasoff />
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    {{ $slot }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('assets/vendors/base/vendor.bundle.base.js') }}"></script>
+
+    <script src="{{ asset('assets/vendors/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+
+    <script src="{{ asset('assets/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('assets/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('assets/js/template.js') }}"></script>
+
+    <script src="{{ asset('assets/js/dashboard.js') }}"></script>
+    <script src="{{ asset('assets/js/data-table.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('assets/js/dataTables.bootstrap4.js') }}"></script>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+</body>
+
+</html>
